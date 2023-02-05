f6ce14b31b7840329284cb91784069dc2455d3de
Pluviolithic <e-mckanna@onu.edu>
2023-02-05 11:34:54 -0500

    Changed to find inverses of all binary circulants

--- src/py/solver.py
+++ src/py/solver.py
@@ -7,31 +7,26 @@ from sympy import Matrix
 from more_itertools import distinct_combinations
 
 
-def generateMatrix(a, b, c, size=24):
+def generateMatrix(combination, size=24):
     m = []
     for i in range(size):
         row = [0] * size
         for j in range(size):
-            if j == ((i + a) % size):
-                row[j] = 1
-            elif j == ((i + b) % size):
-                row[j] = 1
-            elif j == ((i + c) % size):
-                row[j] = 1
-            else:
-                row[j] = 0
+            for k in combination:
+                if j == ((i + k) % size):
+                    row[j] = 1
+                    break
         m.append(row[::-1])
 
     return Matrix(np.array(m[::-1]))
 
 
-def findMatrixInverse(i, j, k, solutions, size=24):
-    m = generateMatrix(i, j, k, size)
+def findMatrixInverse(combination, solutions, size=24):
+    m = generateMatrix(combination, size)
 
     try:
         inv = np.array(m.inv_mod(2)).astype(np.float64)
     except:
-        print("value error: ", i, j, k)
         return
 
     indices = []
@@ -39,30 +34,27 @@ def findMatrixInverse(i, j, k, solutions, size=24):
         if inv[size - 1][::-1][x] == 1:
             indices.append(str(x))
 
-    solutions[", ".join(map(str, [i, j, k]))] = ", ".join(indices)
+    solutions[", ".join(map(str, combination))] = ", ".join(indices)
 
 
 def inverseAll(solutions, size=24):
-    combinations = distinct_combinations([x for x in range(size)], 3)
-    for combination in combinations:
-        i, j, k = combination
-        if i == j and j == k:
-            continue
-        findMatrixInverse(i, j, k, solutions, size)
+    for n in range(size):
+        combinations = distinct_combinations([x for x in range(size)], n)
+        for combination in combinations:
+            findMatrixInverse(combination, solutions, size)
 
 
-for i in range(4, 100):
+for i in range(2, 100):
     solutions = {}
 
-    solutions["size"] = 0
     inverseAll(solutions, i)
-    solutions["size"] = len(solutions) - 1
+    for key, value in solutions.items():
+            print(key, " -> ", value)
+            
+    solutions["size"] = len(solutions)
 
     git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
     git_root = git_repo.git.rev_parse("--show-toplevel")
 
     with open(git_root + "\\json\\py_out\\" + str(i) + ".json", "w") as outfile:
         json.dump(solutions, outfile, indent=4)
-
-        for key, value in solutions.items():
-            print(key, " -> ", value)
